7a2b9c109052cd2b37278bddec3cd6e8e4954023
Dinesh T <dinesh.tikai@student.hu.nl>
2022-02-22 15:04:10 +0100

    Added practicum4A

---
+++ src/practicum4A/Huis.java
@@ -0,0 +1,27 @@
+package practicum4A;
+
+public class Huis {
+    private String adres;
+    private int bouwjaar;
+    private Persoon huisbaas;
+
+    public Huis(String adr, int bwjr) {
+        this.adres = adr;
+        this.bouwjaar = bwjr;
+    }
+
+    public void setHuisbaas(Persoon hb) {
+        this.huisbaas = hb;
+    }
+
+    public Persoon getHuisbaas() {
+        return huisbaas;
+    }
+
+    @Override
+    public String toString() {
+        return "Huis " + adres +
+                " is gebouwd in " + bouwjaar +
+                "\nen heeft huisbaas " + huisbaas;
+    }
+}
---
+++ src/practicum4A/Persoon.java
@@ -0,0 +1,16 @@
+package practicum4A;
+
+public class Persoon {
+    private String naam;
+    private int leeftijd;
+
+    public Persoon(String nm, int lft) {
+        this.naam = nm;
+        this.leeftijd = lft;
+    }
+
+    @Override
+    public String toString(){
+        return naam + "; leeftijd " + leeftijd + " jaar";
+    }
+}
